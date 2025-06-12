4cf7257b5ab8eb947b1c20aa5700419fdc8edf43
Simran Singh <m.simx2909@gmail.com>
2025-06-12 22:37:59 +0530

    Use config paths

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,19 +1,24 @@
 import os
 import torch
-from torchvision import datasets, transforms
+from torchvision import datasets
 from torch.utils.data import DataLoader
+from src.utils import get_train_transforms, get_val_test_transforms
+from src import config
 
-def get_data_loaders(batch_size=32):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+def get_data_loaders(batch_size=config.BATCH_SIZE):
+    train_transform = get_train_transforms()
+    val_transform = get_val_test_transforms()
 
-    train_dataset = datasets.ImageFolder(root=r'D:\JetBrains\PyCharm Professional\MediPrediction\data\train', transform=transform)
-    val_dataset = datasets.ImageFolder(root=r'D:\JetBrains\PyCharm Professional\MediPrediction\data\val', transform=transform)
+    train_dataset = datasets.ImageFolder(root=config.TRAIN_DIR, transform=train_transform)
+    val_dataset = datasets.ImageFolder(root=config.VAL_DIR, transform=val_transform)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, val_loader
+
+def get_test_loader(batch_size=config.BATCH_SIZE):
+    test_transform = get_val_test_transforms()
+    test_dataset = datasets.ImageFolder(root=config.TEST_DIR, transform=test_transform)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return test_loader
